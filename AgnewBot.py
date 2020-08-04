6a9e4a705a135d847483ce8de567dea3bc622c04
B-Woody <woody.skate.and.destroy@gmail.com>
2020-08-04 10:29:18 +1000

    Fixed bug where current_time variable wasn't being set correctly

--- AgnewBot.py
+++ AgnewBot.py
@@ -5,6 +5,10 @@ from fbchat import Client
 from fbchat.models import *
 
 def sendit():
+    current_time = datetime.now().strftime("%-I:%M")
+    message = 'Yo ' + person + ' it is ' \
+         + str(current_time) + \
+         " and I'm thinking of you ❤️ "
     client.send(Message(text=message), thread_id=thread.uid, thread_type=ThreadType.USER)
 
 print('Please enter Facebook creds to continue \n')
@@ -14,20 +18,14 @@ person = input("\nWho are we chatting with:")
 thread = client.searchForThreads(person)[0]
 print('The UID of ' + person + ' is ' + str(thread.uid))
 
-firstmessagetime = "9:00"
-secondmessagetime = "17:00"
-current_time = datetime.now().strftime("%-I:%M")
-#song = "placeholder"
- 
-message = 'TEST yo ' + person + ' it is ' \
-         + str(current_time) + \
-         " and I'm thinking of you ❤️ " 
-
-sendit()
+firstmessagetime = "09:00"
+secondmessagetime = "16:20"
 
 schedule.every().day.at(firstmessagetime).do(sendit)
 schedule.every().day.at(secondmessagetime).do(sendit)
 while True:
     schedule.run_pending()
     time.sleep(10)
+
+
 
